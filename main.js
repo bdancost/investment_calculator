723a7cddbc383f7ccc316b101c8b5881f9079b8f
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-06 20:47:06 -0300

    Adicionando o Evento de Blur e detencao de erros

--- main.js
+++ main.js
@@ -5,16 +5,20 @@ const form = document.getElementById("investment-form");
 function renderProgression(evt) {
   evt.preventDefault();
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const addtionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -26,6 +30,31 @@ function renderProgression(evt) {
   );
 
   console.log(returnsArray);
+}
+
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add("text-red-500");
+    errorTextElement.innerText = "Insira um valor numérico e maior que zero.";
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
 }
 
 form.addEventListener("submit", renderProgression);
